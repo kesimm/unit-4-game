8dbfddbef5b9933c41a137e0abe5b4f4326cab96
kz1127 <kenzie.simmons27@gmail.com>
2018-05-06 18:20:58 -0600

    done for now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,48 +6,84 @@ function getRandomInt(min, max) {
 
 var wins=0;
 var losses=0;
+var randomNumber
+var blue
+var white
+var green
+var red
+var score
 
 function setUp(){    
-var randomNumber = getRandomInt(19, 120);
-var blue = getRandomInt(1,12);
-var white = getRandomInt(1,12);
-var green = getRandomInt(1,12);
-var red = getRandomInt(1,12);
-var score=0;
-    
-$(".randomNumber").text("Your Random Number is: " + randomNumber);
+randomNumber = getRandomInt(19, 120);
+blue = getRandomInt(1,12);
+white = getRandomInt(1,12);
+green = getRandomInt(1,12);
+red = getRandomInt(1,12);
+score=0;
 $(".wins").text("Wins: " + wins);
 $(".losses").text("Losses: " + losses);
+$(".randomNumber").text("Your Random Number is: " + randomNumber);
 $(".score").text(score);
+}
 
+setUp();
+    
 $("#one").on("click",function(){
     score=(score+blue);
-    $(".score").text(score);    
+    $(".score").text(score);
+    if(score===randomNumber){
+        wins++;
+        alert("You Win!")
+        setUp();
+    } else if(score > randomNumber){
+        losses++;
+        alert("You Lose")
+        setUp();
+    }       
 })
 
 $("#two").on("click",function(){
     score=(score+white);
-     $(".score").text(score); 
-    console.log(white);  
+     $(".score").text(score);
+     if(score===randomNumber){
+        wins++;
+        alert("You Win!")
+        setUp();
+    } else if(score > randomNumber){
+        losses++;
+        alert("You Lose")
+        setUp();
+    }    
 })
 
 $("#three").on("click",function(){
     score=(score+green);
-    $(".score").text(score);    
+    $(".score").text(score);
+    if(score===randomNumber){
+        wins++;
+        alert("You Win!")
+        setUp();
+    } else if(score > randomNumber){
+        losses++;
+        alert("You Lose")
+        setUp();
+    }   
  })
 
 $("#four").on("click",function(){
     score=(score+red);
     $(".score").text(score);    
-})
-
-
-
+    if(score===randomNumber){
+    wins++;
+    alert("You Win!")
+    setUp();
+} else if(score > randomNumber){
+    losses++;
+    alert("You Lose")
+    setUp();
 }
 
-setUp();
-
-
+})
 
 
 
